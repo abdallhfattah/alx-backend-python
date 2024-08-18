508a5f4d0d3506d86a5b6d3567d0ccc200f4179e
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-18 18:03:04 +0300

    element length

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Complex types - string and int/float to tuple"""
-from typing import List, Tuple, Sequence , Iterable
+from typing import List, Tuple, Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """ Element length """
+    """Element length"""
     return [(i, len(i)) for i in lst]
