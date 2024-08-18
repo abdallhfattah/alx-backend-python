8e01814b39c99e2970f1ea9bfd4b3c2bdb0b3eb5
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-18 17:47:39 +0300

    summing list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-from typing import List , Union
+from typing import List, Union
 
 """
     Basic annotations for variables.
 """
 
 
-def sum_mixed_list(a: List[Union[int , float]]) -> float:
+def sum_mixed_list(a: List[Union[int, float]]) -> float:
     """
     Takes a list input_list of floats as argument
     returns their sum as a float.
