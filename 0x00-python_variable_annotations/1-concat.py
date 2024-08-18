4bdabe77fde0ad7847fffab9aed85908ccd44b3f
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-18 15:10:05 +0300

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+"""
+    Basic annotations for variables.
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """
+    Returns concatenation of a and b.
+    """
+    return a + b
