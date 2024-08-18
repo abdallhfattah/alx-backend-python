eb5b82bcc8c16da2127b18e6a1e0e46b1d550a3f
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-18 17:42:45 +0300

    summing list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+"""
+    Basic annotations for variables.
+"""
+
+
+def sun_list(a: list[float]) -> float:
+    """
+    Takes a list input_list of floats as argument
+    returns their sum as a float.
+    """
+    return sum(a)
