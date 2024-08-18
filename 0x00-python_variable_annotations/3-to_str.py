df1df8d0b354a3fdbae1dc9bcf8787514a6979f4
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-18 16:04:01 +0300

    to_str

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+"""
+    Basic annotations for variables.
+"""
+
+
+def to_str(a: float) -> str:
+    """
+    Returns string representation of a
+    """
+    return a.__str__()
