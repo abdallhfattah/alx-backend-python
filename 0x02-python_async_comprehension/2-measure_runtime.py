1d77816a0aca0004af83b1538690f669f18806bb
abdallhfattah <abdallh.fattah33@gmail.com>
2024-09-01 12:30:56 +0300

    measure time

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+""" documenting """
 import typing
 import time
 import asyncio
@@ -8,6 +8,8 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
+    """measuring run time of async comprehension function"""
+
     start = time.time()
     waiting_list = await async_comprehension()
     return time.time() - start
